026d5eb427fb1b35c95f6532948fd5ebe32a4690
Brandon Piper <brandonpiper1@gmail.com>
2021-11-16 19:01:41 +0000

    comment=changes

--- Assets/gitPush.bat
+++ Assets/gitPush.bat
@@ -4,7 +4,7 @@ set /p msg=Enter Message:
 
 git add ./
 git pull
-git push --repo https://github.com/B33bo/AZgame.git
 git commit -u -m "comment=%msg%"
+git push --repo https://github.com/B33bo/AZgame.git
 
 SET /p dummy = "finished."
