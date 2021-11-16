b8380402c2f810a4d514049d1fba8d989f8425af
Brandon Piper <brandonpiper1@gmail.com>
2021-11-16 19:02:09 +0000

    git push script works now

--- Assets/gitPush.bat
+++ Assets/gitPush.bat
@@ -4,7 +4,7 @@ set /p msg=Enter Message:
 
 git add ./
 git pull
-git commit -u -m "comment=%msg%"
+git commit -u -m "%msg%"
 git push --repo https://github.com/B33bo/AZgame.git
 
 SET /p dummy = "finished."
